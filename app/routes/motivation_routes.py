a291d4328eace0a8600e677c5ee6c972c9f99c67
rahelpsrb <rahelpasaribu02@gmail.com>
2026-04-23 10:03:43 +0700

    init

--- app/routes/motivation_routes.py
+++ app/routes/motivation_routes.py
@@ -9,31 +9,31 @@ motivation_bp = Blueprint("motivation", __name__)
 @motivation_bp.route("/", methods=["GET"])
 def index():
     return "API telah berjalan! Dibuat oleh Rahel Cantik!"
-    
 
-@motivation_bp.route("/fashion/generate", methods=["POST"])
+
+@motivation_bp.route("/book/generate", methods=["POST"])
 def generate():
     data = request.get_json()
-    skin_tone = data.get("skin_tone")
+    genre = data.get("genre")
     total = data.get("total")
 
-    if not skin_tone:
-        return jsonify({"error": "Skin tone is required"}), 400
-    
+    if not genre:
+        return jsonify({"error": "Genre is required"}), 400
+
     if not total:
         return jsonify({"error": "Total is required"}), 400
-    
+
     if total <= 0:
         return jsonify({"error": "Total harus besar dari 0"}), 400
-    
+
     if total > 10:
         return jsonify({"error": "Total maksimal harus 10"}), 400
 
     try:
-        result = create_fashion_advice(skin_tone, total)
+        result = create_book_recommendations(genre, total)
 
         return jsonify({
-            "skin_tone": skin_tone,
+            "genre": genre,
             "total": len(result),
             "data": result
         })
@@ -42,11 +42,11 @@ def generate():
         return jsonify({"error": str(e)}), 500
 
 
-@motivation_bp.route("/fashion", methods=["GET"])
+@motivation_bp.route("/book", methods=["GET"])
 def get_all():
     page = request.args.get("page", default=1, type=int)
     per_page = request.args.get("per_page", default=100, type=int)
 
-    data = get_all_fashion_advice(page=page, per_page=per_page)
+    data = get_all_book_recommendations(page=page, per_page=per_page)
 
     return jsonify(data)
